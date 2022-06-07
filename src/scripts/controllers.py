0db4ea2926813b9b292a686cf1befefb34af74fd
PabloGalvis <pablo.galvis@cumplimos.co>
2022-06-07 09:10:24 -0500

    paciente y diagnostico

--- src/scripts/controllers.py
+++ src/scripts/controllers.py
@@ -9,7 +9,103 @@ http = urllib3.PoolManager()
 query_helper = Query()
 
 
+def insertPasientes():
+    try:
+        if not request.json:
+            raise Exception("Request con body vacio")
+        Data = request.json
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.insertar("paciente",Data)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
 
+def getPasientes():
+    try:
+        args=list(request.args.items())
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.mostrar(args)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
+
+def modificarPacientes():
+    try:
+        if not request.json:
+            raise Exception("Request con body vacio")
+        args=list(request.args.items())
+        Data = request.json
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.modificar("paciente",args ,Data)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
+
+def listaFuntionPaciente():
+    if request.method == "POST":
+        return insertPasientes()
+    elif request.method == "GET":
+        return getPasientes()
+    elif request.method == "PUT":
+        return modificarPacientes()
+
+def insertarDiagnostico():
+    try:
+        if not request.json:
+            raise Exception("Request con body vacio")
+        Data = request.json
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.insertar("diagnostico",Data)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
+
+def getDiagnosticos():
+    try:
+        args=list(request.args.items())
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.buscar("diagnostico",args)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
+
+def modificarDiagnostico():
+    try:
+        if not request.json:
+            raise Exception("Request con body vacio")
+        args=list(request.args.items())
+        Data = request.json
+    except Exception as e :
+        return f"{e}"
+    
+    try:
+        query=query_helper.modificar("diagnostico",args ,Data)
+        return jsonify(query)
+    except Exception as e:
+        return f" error {e}"
+
+def listaFuntionDiagnostico():
+    if request.method == "POST":
+        return insertarDiagnostico()
+    elif request.method == "GET":
+        return getDiagnosticos()
+    elif request.method == "PUT":
+        return modificarDiagnostico()
 
 def get_pasillos():
     try:
--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -7,9 +7,9 @@ banca = Blueprint('banca', __name__)
 banca.add_url_rule(
     '/doctor', view_func=controllers.doctor, methods=['GET,POST,PUT'])
 banca.add_url_rule(
-    '/paciente', view_func=controllers.paciente, methods=['GET,POST,PUT'])
+    '/paciente', view_func=controllers.listaFuntionPaciente, methods=['GET,POST,PUT'])
 banca.add_url_rule(
-    '/diagnostico', view_func=controllers.diagnostico, methods=['GET,POST,PUT'])
+    '/diagnostico', view_func=controllers.listaFuntionDiagnostico, methods=['GET,POST,PUT'])
 
 
 
