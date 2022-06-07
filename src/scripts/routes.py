af15aa5beef5fa822e52cc1a95473b23eb0b28ba
PabloGalvis <pablo.galvis@cumplimos.co>
2022-06-07 10:00:10 -0500

    areglo back2

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -4,19 +4,13 @@ from . import controllers
 banca = Blueprint('banca', __name__)
 
 banca.add_url_rule(
-<<<<<<< HEAD
-    '/doctor', view_func=controllers.doctor, methods=['GET,POST,PUT'])
+    '/doctor', view_func=controllers.cruDoctor, methods=['GET', 'POST', 'PUT'])
 banca.add_url_rule(
     '/paciente', view_func=controllers.listaFuntionPaciente, methods=['GET,POST,PUT'])
 banca.add_url_rule(
     '/diagnostico', view_func=controllers.listaFuntionDiagnostico, methods=['GET,POST,PUT'])
-
-=======
-    '/doctor', view_func=controllers.cruDoctor, methods=['GET', 'POST', 'PUT'])
 banca.add_url_rule(
      '/especializacion', view_func=controllers.cruEspecializacion, methods=['POST,PUT'])
-# banca.add_url_rule(
-#     '/diagnostico', view_func=controllers.diagnostico, methods=['GET,POST,PUT'])
->>>>>>> 3b6fed489ad402a610b9f47512649727c218291a
+
 
 
