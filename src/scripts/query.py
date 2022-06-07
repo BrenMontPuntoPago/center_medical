3b6fed489ad402a610b9f47512649727c218291a
Brenda Montero <brenda.montero@cumplimos.co>
2022-06-07 08:55:06 -0500

    Cambios back completed

--- src/scripts/query.py
+++ src/scripts/query.py
@@ -8,19 +8,14 @@ class Query(Connection):
 
     # --------------- CONSUMO A QUERY DE BASE DE DATOS --------------------
 
-    def mostrar(self):
+    def doctor(self):
         cnx = self.connect()
         cursor = cnx.cursor()
         # Define el inicio de la query
         try:
-            query = f"""SELECT *
-FROM paciente
-INNER JOIN  diagnostico ON diagnostico.fk_pac_id= paciente.pk_pac_id
-INNER JOIN paciente_doctor ON paciente_doctor.pk_pac_id= paciente.pk_pac_id
-INNER JOIN doctor ON paciente_doctor.pk_doctor_id= doctor.pk_doctor_id
-INNER JOIN especializacion ON especializacion.fk_doctor= doctor.pk_doctor_id
-
-;"""
+            query = f"""SELECT * FROM doctor
+            INNER JOIN especializacion ON especializacion.fk_doctor_id= doctor.pk_doctor_id
+            ;"""
             cursor.execute(query)
             cnx.commit()
             lista = [dict((cursor.description[i][0], value)
@@ -31,36 +26,54 @@ INNER JOIN especializacion ON especializacion.fk_doctor= doctor.pk_doctor_id
             print(e)
             return "prueba"
 
-    def insertar(self, tabla ,datoModificar):
+    def doctorInsert(self, datos):
         cnx = self.connect()
         cursor = cnx.cursor()
-        var=list(datoModificar)
-        datos=[]
-        for k in datoModificar:
-            datos.append(str(datoModificar[str(k)]))
-
-        var_text=", ".join(var)
-        datos_text="','".join(datos)
-
-        
-        
+        # Define el inicio de la query
         try:
-            query= f""" INSERT INTO {tabla} ({var_text}) VALUES ('{datos_text}') RETURNING * ;"""
+            query = f"""INSERT INTO public.doctor(
+                pk_doctor_id, nombre, apellido)
+                VALUES ({datos['pk_doctor_id']}, '{datos['nombre']}', '{datos['apellido']}')
+                RETURNING *
+                ;
+                ;"""
             print(query)
             cursor.execute(query)
             cnx.commit()
-            lista = [dict((cursor.description[i][0], value) \
-               for i, value in enumerate(row)) for row in cursor.fetchall()]
+            lista = [dict((cursor.description[i][0], value)
+                          for i, value in enumerate(row)) for row in cursor.fetchall()]
             self.closeConnection(cnx)
+            print(lista)
             return lista
         except Exception as e:
             print(e)
-            return f"error {e}"
-
-    def modificar(self, tabla,datosBuscar, datoModificar):
+            return "error doctorInsert"
+    def especializacionInsert(self, datos):
         cnx = self.connect()
         cursor = cnx.cursor()
+        # Define el inicio de la query
+        try:
+            query = f"""INSERT INTO public.especializacion(
+                pk_esp_id, descripcion, fk_doctor_id)
+                VALUES ({datos['pk_esp_id']}, '{datos['descripcion']}', '{datos['pk_doctor_id']}')
+                RETURNING *
+                ;
+                ;"""
+            print(query)
+            cursor.execute(query)
+            cnx.commit()
+            lista = [dict((cursor.description[i][0], value)
+                          for i, value in enumerate(row)) for row in cursor.fetchall()]
+            self.closeConnection(cnx)
+            print(lista)
+            return lista
+        except Exception as e:
+            print(e)
+            return "error doctorInsert"
 
+    def update(self, tabla,datosBuscar, datoModificar):
+        cnx = self.connect()
+        cursor = cnx.cursor()
         try:
             for k in datoModificar:
                 query= f""" UPDATE {tabla} SET  {  f"{k} = '{str(datoModificar[str(k)])}'"}   WHERE {datosBuscar[0][0]} = {datosBuscar[0][1]} RETURNING * ;"""
@@ -74,3 +87,4 @@ INNER JOIN especializacion ON especializacion.fk_doctor= doctor.pk_doctor_id
         except Exception as e:
             print(e)
             return f"error {e}"
+    
--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -3,13 +3,12 @@ from . import controllers
 
 banca = Blueprint('banca', __name__)
 
+banca.add_url_rule(
+    '/doctor', view_func=controllers.cruDoctor, methods=['GET', 'POST', 'PUT'])
 
-banca.add_url_rule(
-    '/doctor', view_func=controllers.doctor, methods=['GET,POST,PUT'])
-banca.add_url_rule(
-    '/paciente', view_func=controllers.paciente, methods=['GET,POST,PUT'])
-banca.add_url_rule(
-    '/diagnostico', view_func=controllers.diagnostico, methods=['GET,POST,PUT'])
-
+# banca.add_url_rule(
+#     '/paciente', view_func=controllers.paciente, methods=['GET,POST,PUT'])
+# banca.add_url_rule(
+#     '/diagnostico', view_func=controllers.diagnostico, methods=['GET,POST,PUT'])
 
 
